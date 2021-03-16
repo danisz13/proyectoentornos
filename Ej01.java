dcfcc0603233a62cbbd9529fdeb3d083f8bc1a80
DANIEL SANZ ARRANZ <sanarrda@gregoriofer.local>
2021-03-16 11:57:50 +0100

    Preuba

---
+++ Ej01.java
@@ -0,0 +1,51 @@
+package EjerciciosT1;
+
+public class Ej01 {
+
+    public static void main(String[] args) {
+
+        //Declaramos las variables
+//        int edad;
+//        int altura; //cm
+//        int peso;   //gramos
+//        
+//        edad=24;
+//        edad=28;
+//        edad=edad+1;
+//        edad=(int) 18.5;
+//        
+//        short peque;
+//        peque=125;
+//        
+//        peque=(short) edad;
+//        
+//        
+//        System.out.println(edad);
+//
+//        float f=(float) 1.0;
+//        f=1.0F;
+//        
+//        double f1=1.0;
+//        
+//        int nota = 1;
+//        boolean aprobado = (nota >= 5);
+//        System.out.println("La variable aprobado es: " + aprobado);
+//
+//        char letraA;
+//        letraA = 'A';
+//        letraA = 65;
+//        System.out.println("La variable letraA vale: " + letraA);
+//        
+//        char letraB;
+//        letraB = 'B';
+//        letraB = 66;
+//        System.out.println("La variable letraA vale: " + letraB);
+//        
+//        char otraLetra;
+//        otraLetra=(char) (letraA+10);
+//        System.out.println("Otra letra: " + otraLetra);
+
+        System.out.println("Hola hoy es \t lunes \n5 de \"octubre\"");
+        
+    }
+}
